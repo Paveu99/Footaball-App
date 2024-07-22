e05bd42c45fec00cf697de64d0de14430815e319
Paweł Jarecki <paweljarecki10@gmail.com>
2024-07-22 21:35:35 +0200

    Players view is being displayed in "Players" view

--- src/views/PlayersBase.tsx
+++ src/views/PlayersBase.tsx
@@ -2,6 +2,7 @@ import { memo } from "react"
 import "../styles/Base.scss"
 import styled from "styled-components"
 import { Headline } from "../components/headline/Headline"
+import { Players } from "./deeper views/Players"
 
 const PlayersView = styled.div`
   background-color: ${(props) => props.theme.colors.secondaryBackground};
@@ -14,6 +15,7 @@ export const PlayersBase = memo(() => {
   return (
     <PlayersView className="base">
       <Headline text="Players" />
+      <Players />
     </PlayersView>
   )
 })
