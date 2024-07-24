ea7e96d7bc8ffdc356f83bd14273f6b88ae4a5df
Paweł Jarecki <paweljarecki10@gmail.com>
2024-07-24 22:49:32 +0200

    Submit button now performs on click functions

--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -4,6 +4,7 @@ import "../../styles/AddPlayerForm.scss"
 type Props = {
   view: string
   disabled: boolean
+  onClick?: () => void
 }
 
 const Button = styled.button`
@@ -11,9 +12,14 @@ const Button = styled.button`
   color: ${(props) => props.theme.colors.primaryTextColor};
 `
 
-export const SubmitButton = ({ view, disabled }: Props) => {
+export const SubmitButton = ({ view, disabled, onClick }: Props) => {
   return (
-    <Button className="submit-button" type="submit" disabled={disabled}>
+    <Button
+      className="submit-button"
+      type="submit"
+      disabled={disabled}
+      onClick={onClick ? () => onClick() : undefined}
+    >
       <strong>{view}</strong>
     </Button>
   )
